4098f379791a25242bf609354f82fb48701d67d7
Alexanderpmann <99506481+Alexanderpmann@users.noreply.github.com>
2022-04-06 06:12:58 -0400

    updating code to correct format/resolve
    per last class, I was confused as to which aspect of the assignment - users, bank_account, or users_with_bank_accounts I was on and ended up jumbling all of them together. 
    
    This is the new code that appears to be correct and is not problematic/lost in thought.

--- Users_with_bank_accounts.py
+++ Users_with_bank_accounts.py
@@ -1,32 +1,44 @@
-from bank import BankAccount
 
 
 class BankAccount:
-
-    def __init__(self, int_rate, balance):
-        self.int_rate = 0.1
-        self.balance = 0
-
+    bank_name = "Doja Bank" 
+    all_accounts = [] 
+    
+    def __init__(self, balance, int_rate): 
+        self.balance = balance
+        self.int_rate = int_rate
     def deposit(self, amount):
-        self.balance += amount
+        self.balance += (amount)
+        return self
+    def withdrawl(self, amount):
+        self.balance -= (amount)
+        return self
+    def display_account_info(self):
+        print(f"BankAccount: ${self.balance}")
+        return self
+    def yield_interest(self):
+        self.balance = self.balance + (self.balance * self.int_rate) 
         return self
 
 
 class User:
-    def __init__(self, data):
-        self.name = data["name"]
-        self.email = dta["email"]
-        self.account = BankAccount()
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount(balance, int_rate)
 
     def make_deposit(self, amount):
-
         self.account.deposit(amount)
-
+        return self
+    def make_withdrawl(self, amount):
+        self.account.withdrawl(amount)
+        return self
+    def display_user_balance(self):
+        print(f"User: {self.name}, Balance: ${self.account.display_account_info}")
+        return self
+    def transfer_money(self, other_user, amount):
+        self.amount -= amount
+        other_user.account += amount
         return self
 
-    def transfer_money(self, other_user, amount):
-
-
-    user1 = User("Guido", "gman@gmail.com")
-    user2 = User("Monty", "m&m@gmail.com")
 
